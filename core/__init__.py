31b77136408d2575c484c1080247ed4a1bc30639
PadmasaliGovardhan <padmasali.govardhan.p@gmail.com>
2025-10-01 22:47:39 +0530

    commit after frontend and backend api integration in index and script files

--- core/__init__.py
+++
@@ -1,7 +0,0 @@
-class Driver:
-    def __init__(self, name, location):
-        self.name = name
-        self.location = location
-
-    def __repr__(self):
-        return f"Driver({self.name} at {self.location})"
